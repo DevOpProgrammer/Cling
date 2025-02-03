b9863032caa65649ff1432605e42875371eec4cc
Alin Panaitiu <alin.panaitiu@gmail.com>
2025-02-03 13:47:04 +0200

    Initial Commit

---
+++ StaticCling/StaticClingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticClingApp.swift
+//  StaticCling
+//
+//  Created by Alin Panaitiu on 03.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct StaticClingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
